fae839aebc617d0c26d7866a964c51ca2d2332ae
Nievl <rezalhbramantara@gmail.com>
2024-09-25 17:58:18 +0700

    Add Faker Factories

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,15 +6,19 @@
         @foreach ($posts as $post)
             <div class="posts mb-5 shadow rounded-md bg-white ">
                 <article class="p-3 pb-0 block">
-                <a href="/posts/{{ $post['slug'] }}" class="block">
-                    <h3 class="font-semibold text-2xl hover:underline hover:text-emerald-600 block">
-                        {{ $post['title'] }}
-                    </h3>
-                </a>
-                        <a href="#" class="font-light">{{ $post['author'] }} - {{ $post->created_at->format('j M Y') }}, {{ $post->created_at->diffForHumans() }}</a>
-                        <p class="text-justify indent-6 pt-2">
-                            {{ Str::limit($post['body'], 70) }}
-                        </p>
+                    <a href="/posts/{{ $post['slug'] }}" class="block">
+                        <h3 class="font-semibold text-2xl hover:underline hover:text-emerald-600 block">
+                            {{ $post['title'] }}
+                        </h3>
+                    </a>
+                    <a href="#" class="font-light">
+                        {{ $post['author'] }} - 
+                        {{ $post->created_at->format('j M Y') }},
+                        {{ $post->created_at->diffForHumans() }}
+                    </a>
+                    <p class="text-justify indent-6 pt-2">
+                        {{ Str::limit($post['body'], 70) }}
+                    </p>
                 </article>
                 <nav class="mt-2 border-t border-slate-300 text-emerald-600 hover:underline rounded">
                     <a href="/posts/{{ $post['slug'] }}" class=" px-3 py-1 block">
